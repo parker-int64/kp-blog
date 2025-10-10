0761155ec47feb381583bbd5af1acf1762462ada
parkerint64 <huxiaochuan2011@hotmail.com>
2025-10-10 18:28:14 +0800

    Modify sidebar

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,7 +1,7 @@
 // .vitepress/config.js
 export default {
     // site level configurations
-    title: 'K.P\'s Personal Blog',
+    title: 'Personal Log',
     description: 'Share something useful.',
 
     themeConfig: {
@@ -15,11 +15,53 @@ export default {
             {
                 text: 'Guide',
                 items: [
-                    // { text: "LiteRT on Raspberry Pi", link: '/Install-LiteRT-on-RPi' },
-                    // { text: "TensorFlow Lite on Raspberry Pi", link: '/Install-TensorFlow-Lite-on-RPi'}
+                    { text: "Getting Started", link: '/guide/getting-started' },
+                ],
+            },
+            {
+                text: 'Espressif',
+                items: [
+                    { text: "ESPHome Migration", link: '/espressif/esphome-migration-guide' }
+                ]
+            },
+            {
+                text: 'Raspberry Pi',
+                items: [
+                    { text: "LiteRT on Raspberry Pi", link: '/rpi/install-litert-on-rpi' },
+                    { text: "Tensorflow Lite on Raspberry Pi", link: '/rpi/install-tensorflow-lite-on-rpi' },
+                ]
+            },
+            {
+                text: 'NVIDIA Jetson',
+                items: [
+                    { text: 'Flash JetPack on Windows (WSL2)', link: '/jetson/flash-jetpack-on-wsl2' },
+                    { text: 'Build Librealsense', link: '/jetson/build-librealsense' },
+                    { text: 'Build Open3D', link: '/jetson/build-open3d' },
+                    { text: 'Build PCL', link: '/jetson/build-pcl' },
+                    { text: 'Build Pytorch', link: '/jetson/build-pytorch' },
+                    { text: 'Build Torchvision', link: '/jetson/build-torchvision' },
+                    { text: 'Backup and Restore', link: '/jetson/backup-and-restore' },
+                ]
+            },
+            {
+                text: 'Miscellaneous',
+                items: [
+                    { text: 'IMX219/477 on Seeed reComputer', link: '/misc/configure-recomputer-imx219-imx477' },
+                    { text: 'Jetson Naming Conventions', link: '/misc/jetson-naming-conventions' },
+                    { text: 'Jetson安装中文输入法（搜狗拼音）', link: '/misc/jetson安装中文输入法' },
+                    { text: 'SDK Manager适配', link: '/misc/SDKManager适配' }
                 ]
             }
-        ]
+        ],
+
+        editLink: {
+            pattern: 'https://github.com/parker-int64/kp-blog/edit/main/docs/:path',
+            text: 'Edit this page on GitHub'
+        },
+        footer: {
+            message: 'Released under the MIT License.',
+            copyright: 'Copyright © 2019-present Kane/Kevin/parker-int64'
+        }
     },
     ignoreDeadLinks: true,
 
